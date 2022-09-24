3a90fc11ec1721178d6213eda28998dc939b88cc
Nguyễn Anh Tú <112753931+atuzzw08@users.noreply.github.com>
2022-09-24 15:25:06 +0700

    Update zxx.py

--- zxx.py
+++ zxx.py
@@ -882,7 +882,6 @@ def tools():
 ############################################################################################## layer():
 def zxx():  
   clear()                                                                  
-    stdout.write("             "+Fore.LIGHTCYAN_EX            +"        ══╦══════════ZXX DDOS ATTACK════════════════════╦══\n")
     stdout.write("             "+Fore.LIGHTCYAN_EX+"╔═════════╩═════════════════════════════════╩═════════╗\n")
     stdout.write("             "+Fore.LIGHTCYAN_EX+"║ "+Fore.LIGHTWHITE_EX   +"        Welcome To The My Tool By Atu 208 "+Fore.LIGHTCYAN_EX  +"       ║\n")
     stdout.write("             "+Fore.LIGHTCYAN_EX+"║ "+Fore.LIGHTWHITE_EX   +"          Type [help] to see the Commands    "+Fore.LIGHTCYAN_EX +"       ║\n")
